128a83759ca3931ef26cde6695876a9fce9c0ce0
TESCODE <teslimagboola09@gmail.com>
2024-11-12 13:31:27 +0100

    working on paystack

--- Controllers/paystackWebhook.js
+++ Controllers/paystackWebhook.js
@@ -9,7 +9,7 @@ const router = express.Router();
 // Middleware to capture raw body
 app.use(express.json({
     verify: (req, res, buf) => {
-        req.rawBody = buf.toString();
+        req.rawBody = buf.toString('utf8');
     }
 }));
 
@@ -17,15 +17,21 @@ router.post('/webhook', async (req, res) => {
     console.log('In webhook route');
     console.log('Raw body:', req.rawBody);
 
-    const hash = crypto.createHmac('sha512', process.env.API_SECRET).update(req.rawBody).digest('hex');
     const paystackSignature = req.headers['x-paystack-signature'];
-    
+    const rawBody = req.rawBody;
+    const hash = crypto.createHmac('sha512', process.env.API_SECRET).update(rawBody).digest('hex');
+
+    if (!rawBody) {
+      console.error("Raw body is undefined.");
+      return res.status(400).send('Bad Request: Raw body missing');
+    }
     console.log("API_SECRET:", process.env.API_SECRET); // Check if API_SECRET is loaded correctly
     console.log("Hash generated:", hash);
     console.log("Paystack signature:", paystackSignature);
 
     if (hash !== paystackSignature) {
         console.log('Invalid hash');
+        console.log('Paystack signature:', paystackSignature, 'Hash:', hash);
         return res.status(400).send('Invalid signature');
     }
 
